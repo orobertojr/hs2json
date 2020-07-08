5e5cac3fe0d231bdb59f73c21de20b6a540622c3
Roberto Junior <robertojunior@Robertos-MacBook-Pro.local>
2020-07-07 23:50:44 -0300

    Finaliza Prettify, PrettyJSON e adiciona Main

--- hs2json/app/Main.hs
+++ hs2json/app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
 import SimpleJSON
+import Prettify
+import PrettyJSON
+
+value = renderJValue $ JObject [("f", JNumber 1), ("q", JBool True)]
 
 main :: IO ()
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = do
+    putStrLn (pretty 10 value)
+    putStrLn (pretty 20 value)
+    putStrLn (pretty 30 value)
